3647bb82837b4dfc9e3f5ed6bd92aee001bddaea
Tushar Gagal <tushargagal1209@gmail.com>
2024-01-08 14:09:16 +0530

    first commit

---
+++ src/customHook/useCurrencyInfo.js
@@ -0,0 +1,16 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-unused-vars */
+import { useEffect, useState } from "react";
+
+function useCurrencyInfo(currency) {
+  const [data, setData] = useState({});
+  let url = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`;
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((res) => setData(res[currency]));
+  }, [currency]);
+  return data;
+}
+
+export default useCurrencyInfo;
